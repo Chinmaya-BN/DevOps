c4fa744a3f54233dd960bc579cbb6b4586ffcd09
Chinmaya BN <96282981+Chinmaya-BN@users.noreply.github.com>
2024-07-25 00:20:04 +0530

    Add files via upload

---
+++ read/src/main/java/read/read/App.java
@@ -0,0 +1,7 @@
+package read.read;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ read/src/main/java/read/read/readjson.java
@@ -0,0 +1,26 @@
+package read.read;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class readjson {
+
+public static void main(String[] args) throws IOException, ParseException {
+
+       JSONParser jsonparser = new JSONParser();
+       FileReader reader = new FileReader(".\\JSON\\student.json");
+       
+       Object obj = jsonparser.parse(reader);
+        JSONObject studentobj = (JSONObject)obj;
+        String fname = (String)studentobj.get("firstname");
+        String lname = (String)studentobj.get("lastname");
+       
+        System.out.println("Firstname:"+fname);
+        System.out.println("Lastname:"+lname);
+}
+
+}
